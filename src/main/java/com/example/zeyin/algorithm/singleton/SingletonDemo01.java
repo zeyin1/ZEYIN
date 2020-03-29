fafa1da448c1b9d752dda76522b667e3c5631992
zeyin1 <you@example.com>
2020-03-29 16:13:14 +0800

    20200329 单例模式

---
+++ src/main/java/com/example/zeyin/algorithm/singleton/SingletonDemo01.java
@@ -0,0 +1,25 @@
+package com.example.zeyin.algorithm.singleton;
+
+/**
+ * @Description: 单例模式-懒汉模式(线程安全)
+ * @Author: zeyin
+ * @Date: 2020年03月29日 15:54:19
+ * @Modify:
+ */
+
+public class SingletonDemo01 {
+    
+    //没有初始化该对象，当实际用到时再创建
+    private static SingletonDemo01 singletonDemo01;
+
+    //构造器私有化
+    private SingletonDemo01(){}
+
+    //方法同步，线程安全；如果没有synchronized，则是线程不安全的懒汉模式
+    public static synchronized SingletonDemo01 getSingletonDemo01(){
+        if(singletonDemo01==null){
+            singletonDemo01=new SingletonDemo01();
+        }
+        return singletonDemo01;
+    }
+}
---
+++ src/main/java/com/example/zeyin/algorithm/singleton/SingletonDemo05.java
@@ -0,0 +1,18 @@
+package com.example.zeyin.algorithm.singleton;
+
+/**
+ * @Description: 单例模式-枚举类
+ * @Author: zeyin
+ * @Date: 2020年03月29日 16:09:59
+ * @Modify:
+ */
+
+public enum SingletonDemo05 {
+    //添加枚举值
+    ;
+
+    //添加枚举方法
+    public void singletonDemo05(){
+
+    }
+}
