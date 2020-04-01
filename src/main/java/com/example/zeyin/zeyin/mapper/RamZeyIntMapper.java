cfbe399832fd7644ccf835549599e142d6b8e3ac
zeyin1 <you@example.com>
2020-04-01 12:48:41 +0800

    20200330 mapper编写

--- src/main/java/com/example/zeyin/zeyin/mapper/RamZeyIntMapper.java
+++ src/main/java/com/example/zeyin/zeyin/mapper/RamZeyIntMapper.java
@@ -1,10 +1,11 @@
 package com.example.zeyin.zeyin.mapper;
 
 import com.example.zeyin.zeyin.pojo.RamZeyInt;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
 /**
- * @Description: todo 用一句话描述
+ * @Description: 数据实体Mapper
  * @Author: zeyin
  * @Date: 2020年03月29日 23:13:32
  * @Modify:
@@ -13,12 +14,12 @@ import org.springframework.stereotype.Component;
 public interface RamZeyIntMapper {
 
     /**
-     * @Description: todo 用一句话描述
+     * @Description: 查询数据
      * @Author: zeyin
      * @Date: 2020/3/29 23:14
      * @Modify:
      * @Param:
      * @Return:
      */
-    RamZeyInt qryRamZeyIntByTest1(String pTest1);
+    RamZeyInt qryRamZeyIntByTest1(@Param("pTest1") String pTest1);
 }
