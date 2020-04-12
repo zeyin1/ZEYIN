c8eb5f44f078ab8e24dd858a518721cbcdded69d
zeyin1 <you@example.com>
2020-04-12 17:56:29 +0800

    20200330 更改配置

--- src/main/java/com/example/zeyin/ZeyinApplication.java
+++ src/main/java/com/example/zeyin/ZeyinApplication.java
@@ -3,6 +3,7 @@ package com.example.zeyin;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
---
+++ src/main/java/com/example/zeyin/zeyin/transaction/TxServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.zeyin.zeyin.transaction;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @Description: todo 用一句话描述
+ * @Author: zeyin
+ * @Date: 2020年04月12日 17:47:37
+ * @Modify:
+ */
+@Service
+public class TxServiceImpl implements TxService {
+    /**
+    * @Description: 用注解实现方法的独立子事务
+    * @Author: zeyin
+    * @Date: 2020/4/12 17:47
+    * @Modify:
+    * @Param:
+    * @Return:
+    */
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Override
+    public void txServiceMethod() {
+        System.out.println("用注解方式实现独立子事务");
+    }
+}
