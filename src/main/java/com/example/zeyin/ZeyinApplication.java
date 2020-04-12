6636368ed6795f542790c08db120b08ddb622636
zeyin1 <you@example.com>
2020-04-12 17:32:47 +0800

    20200330 更改配置

--- src/main/java/com/example/zeyin/ZeyinApplication.java
+++ src/main/java/com/example/zeyin/ZeyinApplication.java
@@ -1,11 +1,12 @@
 package com.example.zeyin;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.example.zeyin.zeyin.mapper")
+@MapperScan(basePackages = "com.example.zeyin.zeyin",annotationClass = Mapper.class)
 public class ZeyinApplication {
 
     public static void main(String[] args) {
