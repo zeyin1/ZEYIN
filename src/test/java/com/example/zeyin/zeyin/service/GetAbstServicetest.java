af2b8c383fc623b86551c7bf93047cd38f7988bd
zeyin1 <you@example.com>
2020-04-19 10:29:55 +0800

    20200330 service注解使用

--- src/test/java/com/example/zeyin/zeyin/service/GetAbstServicetest.java
+++ src/test/java/com/example/zeyin/zeyin/service/GetAbstServicetest.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
- * @Description: todo 用一句话描述
+ * @Description: 用一句话描述
  * @Author: zeyin
  * @Date: 2020年04月12日 17:19:36
  * @Modify:
@@ -15,13 +15,10 @@ public class GetAbstServicetest {
     @Autowired
     private GetAbstService getAbstService;
     /**
-    * @Description: todo 用一句话描述
-    * @Author: zeyin
-    * @Date: 2020/4/12 17:23
-    * @Modify:
-    * @Param: 
-    * @Return: 
-    */
+     * @Description: 用一句话描述
+     * @Author: zeyin
+     * @Date: 2020/4/19 10:29
+     */
     @Test
     public void test1(){
         getAbstService.getAbstService();
