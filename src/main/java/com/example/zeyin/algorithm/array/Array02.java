b389a15d53932c27a1e692b68400e262b8754e4c
zeyin1 <you@example.com>
2020-05-11 22:27:15 +0800

    20200511 算法

--- src/main/java/com/example/zeyin/algorithm/array/Array02.java
+++ src/main/java/com/example/zeyin/algorithm/array/Array02.java
@@ -1,5 +1,7 @@
 package com.example.zeyin.algorithm.array;
 
+import com.example.zeyin.algorithm.util.ListNode;
+
 import java.util.*;
 
 /**
@@ -415,5 +417,36 @@ public class Array02 {
         return stack.isEmpty();
     }
 
-    //20200511
+    /**
+     * @Description: 21. 合并两个有序链表
+     * 将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+     * @Author: zeyin
+     * @Date: 2020/5/11 22:23
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode list=new ListNode(0);
+        ListNode head=list;
+
+        while(l1!=null&&l2!=null) {
+            if(l1.val<=l2.val) {
+                head.next=l1;
+                l1=l1.next;
+            }else {
+                head.next=l2;
+                l2=l2.next;
+            }
+            head=head.next;
+        }
+        while(l1!=null) {
+            head.next=l1;
+            l1=l1.next;
+            head=head.next;
+        }
+        while(l2!=null) {
+            head.next=l2;
+            l2=l2.next;
+            head=head.next;
+        }
+        return list.next;
+    }
 }
---
+++ src/main/java/com/example/zeyin/algorithm/util/ListNode.java
@@ -0,0 +1,17 @@
+package com.example.zeyin.algorithm.util;
+
+/**
+ * @Description: 用一句话描述
+ * @Author: zeyin
+ * @Date: 2020年05月11日 22:24
+ * @Modify:
+ */
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
