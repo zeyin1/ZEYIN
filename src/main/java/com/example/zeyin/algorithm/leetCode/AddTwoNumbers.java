4e278dcbba3cdee393a8e21d4a8a92ad135fb4bf
zeyin1 <you@example.com>
2020-09-21 21:55:48 +0800

    20200511 算法

---
+++ src/main/java/com/example/zeyin/algorithm/leetCode/AddTwoNumbers.java
@@ -0,0 +1,36 @@
+package com.example.zeyin.algorithm.leetCode;
+
+import com.example.zeyin.algorithm.util.ListNode;
+
+/**
+ * @Description: 用一句话描述
+ * @Author: zeyin
+ * @Date: 2020年09月21日 21:42
+ * @Modify:
+ */
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        int carry = 0;
+        while(l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+
+            cur = cur.next;
+            if(l1 != null)
+                l1 = l1.next;
+            if(l2 != null)
+                l2 = l2.next;
+        }
+        if(carry == 1) {
+            cur.next = new ListNode(carry);
+        }
+        return pre.next;
+    }
+}
