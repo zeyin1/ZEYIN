feb1586a6b8161860eed561700e6814fb3c11cb7
zeyin1 <you@example.com>
2020-11-19 21:07:32 +0800

    20201119 常用遍历

--- src/main/java/com/example/zeyin/algorithm/treeNodeFind/BfsFind.java
+++ src/main/java/com/example/zeyin/algorithm/treeNodeFind/BfsFind.java
@@ -1,10 +1,45 @@
 package com.example.zeyin.algorithm.treeNodeFind;
 
+import com.example.zeyin.util.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
+
 /**
- * @Description: 用一句话描述
+ * @Description: 层次遍历
  * @Author: zeyin
  * @Date: 2020年11月19日 20:02
  * @Modify:
  */
 public class BfsFind {
+
+    /**
+     * 层次遍历用队列实现
+     */
+
+    private static List<TreeNode> list;
+    public static void bfsFind(TreeNode root) {
+        if (root==null){
+            return;
+        }
+
+        Queue<TreeNode> queue=new ArrayDeque<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()){
+            TreeNode node=queue.poll();
+            list.add(node);
+
+            if(node.left!=null){
+                queue.add(node.left);
+            }
+
+            if (node.right!=null){
+                queue.add(node.right);
+            }
+
+        }
+
+    }
 }
--- src/main/java/com/example/zeyin/algorithm/treeNodeFind/DfsFind.java
+++ src/main/java/com/example/zeyin/algorithm/treeNodeFind/DfsFind.java
@@ -1,5 +1,10 @@
 package com.example.zeyin.algorithm.treeNodeFind;
 
+import com.example.zeyin.util.TreeNode;
+
+import java.util.List;
+import java.util.Stack;
+
 /**
  * @Description: 深度遍历
  * @Author: zeyin
@@ -7,4 +12,32 @@ package com.example.zeyin.algorithm.treeNodeFind;
  * @Modify:
  */
 public class DfsFind {
+    /**
+     * 深度优先搜索用栈实现，先加右子树入栈，其次左子树入栈
+     */
+    private static List<TreeNode> list;
+
+    public static void dfsFind(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node);
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+
+        }
+    }
+
 }
