0f525806a1505bc0a3ac0c110abd3fcb536b8e58
zeyin1 <you@example.com>
2020-11-20 22:15:29 +0800

    20201119 常用遍历

---
+++ src/main/java/com/example/zeyin/algorithm/leetCode/leetcode15.java
@@ -0,0 +1,50 @@
+package com.example.zeyin.algorithm.leetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Description: 三数之和
+ * @Author: zeyin
+ * @Date: 2020年11月20日 21:40
+ * @Modify:
+ */
+public class leetcode15 {
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> resList=new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i=0;i<nums.length-2;i++){
+            //去重
+            if (i>0&&nums[i]==nums[i-1]){
+                continue;
+            }
+
+            int l=i+1;
+            int r=nums.length-1;
+            int sum= -nums[i];
+            while (l<r){
+                if (nums[l]+nums[r]==sum){
+                    resList.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                //去重
+                while (l<r&&nums[l]==nums[l+1]){
+                    l++;
+                }
+                while (r>l&&nums[r]==nums[r-1]){
+                    r--;
+                }
+                //下一个元素
+                l++;
+                r--;
+                }else if (nums[l]+nums[r]<sum){
+                    l++;
+                }else {
+                    r--;
+                }
+            }
+        }
+        return resList;
+    }
+}
---
+++ src/main/java/com/example/zeyin/algorithm/leetCode/leetcode670.java
@@ -0,0 +1,13 @@
+package com.example.zeyin.algorithm.leetCode;
+
+/**
+ * @Description: 最大交换
+ * @Author: zeyin
+ * @Date: 2020年11月20日 22:14
+ * @Modify:
+ */
+public class leetcode670 {
+    public int maximumSwap(int num) {
+        return 0;
+    }
+}
