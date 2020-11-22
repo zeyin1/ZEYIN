9e5084a4c3ccf72fb01b14cd5161ef8019f184de
zeyin1 <you@example.com>
2020-11-22 22:07:39 +0800

    20201119 常用遍历

---
+++ src/main/java/com/example/zeyin/algorithm/leetCode/leetcode50.java
@@ -0,0 +1,32 @@
+package com.example.zeyin.algorithm.leetCode;
+
+/**
+ * @Description: 用一句话描述
+ * @Author: zeyin
+ * @Date: 2020年11月22日 21:52
+ * @Modify:
+ */
+public class leetcode50 {
+
+    public double myPow(double x, int n) {
+        //n正负
+        if(n<0)
+            return 1/power(x,-n);
+        else
+            return power(x,n);
+    }
+
+    public double power(double x,int n) {
+        if(n==0)
+            return 1;
+        //二分查找
+        double v=power(x,n/2);
+
+        if(n%2==0)
+            //偶数
+            return v*v;
+        else
+            //奇数
+            return v*v*x;
+    }
+}
---
+++ src/main/java/com/example/zeyin/algorithm/leetCode/leetcode560.java
@@ -0,0 +1,39 @@
+package com.example.zeyin.algorithm.leetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description: 用一句话描述
+ * @Author: zeyin
+ * @Date: 2020年11月22日 21:55
+ * @Modify:
+ */
+public class leetcode560 {
+
+    //动态规划
+
+    /**
+     *pre[i]代表i之前（包括i）的和
+     * 0<j<i
+     * pre[i]-pre[j]=k
+     */
+    public int subarraySum(int[] nums, int k) {
+        int count=0;
+        int pre=0;
+        Map<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+
+        for (int i=0;i<nums.length;i++){
+            pre=pre+nums[i];
+
+            if (map.containsKey(pre-k)){
+                count+=map.get(pre-k);
+            }
+
+            map.put(pre,map.getOrDefault(pre,0)+1);
+        }
+        return count;
+
+    }
+}
