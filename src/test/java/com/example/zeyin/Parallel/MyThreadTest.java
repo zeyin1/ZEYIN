cb9633db1c1df5b0208217c5e368c32fff5aa342
zeyin1 <you@example.com>
2021-01-10 15:34:38 +0800

    20201119 常用遍历

--- src/test/java/com/example/zeyin/Parallel/MyThreadTest.java
+++ src/test/java/com/example/zeyin/Parallel/MyThreadTest.java
@@ -22,4 +22,40 @@ public class MyThreadTest {
         Boolean res = myThread3.call();
         System.out.println("call方法返回的结果：" + res);
     }
+
+    /**
+     * @Description: 一个线程不能多次start
+     * @Author: zeyin
+     * @Date: 2021/1/10 15:30
+     */
+    @Test
+    public void myThreadTest2() {
+        MyThread2 myThread2 = new MyThread2();
+        myThread2.start();
+        myThread2.start();
+    }
+
+    /**
+     * @Description: 一个可以多次run
+     * @Author: zeyin
+     * @Date: 2021/1/10 15:32
+     */
+    @Test
+    public void myThreadTest21() {
+        MyThread2 myThread2 = new MyThread2();
+        myThread2.run();
+        myThread2.run();
+    }
+    @Test
+    public void myThreadTest3() {
+        MyThread1 myThread1 = new MyThread1();
+        myThread1.run();
+        myThread1.run();
+    }
+    @Test
+    public void myThreadTest4() {
+        MyThread3 myThread3 = new MyThread3();
+        myThread3.call();
+        myThread3.call();
+    }
 }
