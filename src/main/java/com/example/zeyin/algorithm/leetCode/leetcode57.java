7e768b2e04fc88f99eca24cb8cdee70aa868fdfa
zeyin1 <you@example.com>
2021-01-24 15:45:25 +0800

    20201119 常用遍历

---
+++ src/main/java/com/example/zeyin/algorithm/leetCode/leetcode57.java
@@ -0,0 +1,13 @@
+package com.example.zeyin.algorithm.leetCode;
+
+/**
+ * @Description: 用一句话描述
+ * @Author: zeyin
+ * @Date: 2021年01月24日 15:44
+ * @Modify:
+ */
+public class leetcode57 {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        return null;
+    }
+}
