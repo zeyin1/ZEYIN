b812f9a599fc2974d9b20a68f20d54c8c94fb0aa
zeyin1 <you@example.com>
2021-02-23 14:26:19 +0800

    20201119 常用遍历

---
+++ src/main/java/com/example/zeyin/algorithm/leetCode/leetcode486.java
@@ -0,0 +1,13 @@
+package com.example.zeyin.algorithm.leetCode;
+
+/**
+ * @Description: 用一句话描述
+ * @Author: zeyin
+ * @Date: 2021年02月23日 14:15
+ * @Modify:
+ */
+public class leetcode486 {
+    public boolean PredictTheWinner(int[] nums) {
+        return true;
+    }
+}
---
+++ src/test/java/com/example/zeyin/algorithm/leetCode/leetcode57Test.java
@@ -0,0 +1,21 @@
+package com.example.zeyin.algorithm.leetCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Description: 用一句话描述
+ * @Author: zeyin
+ * @Date: 2020年11月21日 10:22
+ * @Modify:
+ */
+@SpringBootTest
+public class leetcode57Test {
+
+    @Test
+    public void test() {
+        int[][] intervals={{1,3},{6,9}};
+        int[] newInterval={2,5};
+        int[][] res=leetcode57.insert(intervals,newInterval);
+    }
+}
